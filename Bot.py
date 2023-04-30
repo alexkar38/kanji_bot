291127c77682bb98c11449625dbf9aa95fa927bd
alexkar38 <a01alex@yandex.ru>
2023-04-30 18:18:31 +0300

    add link

--- Bot.py
+++ Bot.py
@@ -1,14 +1,16 @@
 import telebot
 import settings
-
+import requests
 
 bot = telebot.TeleBot(settings.TELEGRAMM_TOKEN)
 
-@bot.message_handler(commands=['start'])
+@bot.message_handler(content_types=['text'])
 
 def start(message):
-    bot.send_message(message.chat.id, 'こんにちわ')
+    url = f'https://kanjiapi.dev/v1/kanji/{message.text}'
+    response = requests.get(url).json()
+    #print(response)
+    bot.send_message(message.chat.id, response)
 
 
 bot.polling(none_stop=True)
-
