e7568f1a127d8352a75b8d86efbac68f86c6e035
alexkar38 <a01alex@yandex.ru>
2023-05-27 14:37:32 +0300

    nothing spec

--- Bot.py
+++ Bot.py
@@ -1,16 +1,24 @@
 import telebot
 import settings
+import json
 import requests
+
 
 bot = telebot.TeleBot(settings.TELEGRAMM_TOKEN)
 
 @bot.message_handler(content_types=['text'])
 
-def start(message):
-    url = f'https://kanjiapi.dev/v1/kanji/{message.text}'
-    response = requests.get(url).json()
-    #print(response)
-    bot.send_message(message.chat.id, response)
+def start(message):    
+    try:
+        url = f'https://kanjiapi.dev/v1/kanji/{message.text}'
+        response = requests.get(url).json() 
+        answer = response['meanings']      
+             
+    except Exception:
+        answer = "error:"     
+    print(answer)    
+    print (response)       
+    bot.send_message(message.chat.id, answer, parse_mode='html')
 
 
 bot.polling(none_stop=True)
